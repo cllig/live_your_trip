0983c44cecfa9ca4977e77e69634b9cc79922d7e
Claire Lignon <claire.lignon@gmail.com>
2022-01-04 14:54:49 +0100

    offer controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,50 @@
 class OffersController < ApplicationController
+  before_action :set_offers, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def show
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(params_offer)
+    @offer.user = current_user
+    if @offer.save
+      redirect_to offers_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @offer.update(params_offer)
+    if @offer.save
+      redirect_to offers_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @offer.destroy
+    redirect_to offers_path
+  end
+
+  private
+
+  def set_offers
+    @offer = Offer.find(params[:id])
+  end
+
+  def params_offer
+    params.require(:offer).permit(:title, :theme, :price, :city, :placenumber, :user_id, photos: [])
+  end
+end
+  
 end
