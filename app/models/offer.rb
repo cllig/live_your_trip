02de30ee63f34f1e0de28a15fd23320a808209b5
laure reina <laure.reina@gmail.com>
2022-01-04 14:54:05 +0100

    validates model user et offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,12 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_many :reservations
 
+  validates :price, presence: true
+  validates :theme, presence: true
+  validates :city, presence: true
+  validates :title, presence: true
+  validates :placenumber, presence: true
+
   include PgSearch::Model
   pg_search_scope :search_by_city_and_theme,
     against: [ :city, :theme ],
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :reservations
   has_many :offers
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :city, presence: true
 end
