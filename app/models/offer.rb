21e1329cffcfa4e3825364fb180a31e53786a89a
laure reina <laure.reina@gmail.com>
2022-01-04 12:45:45 +0100

    debut

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,10 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many :reservations
+
+  include PgSearch::Model
+  pg_search_scope :search_by_city_and_theme,
+    against: [ :city, :theme ],
+    using: {
+      tsearch: { prefix: true }
 end
