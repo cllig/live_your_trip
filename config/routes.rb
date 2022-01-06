8c4ad64cf47274610e3a98564a3fe6f5dd098290
Claire Lignon <claire.lignon@gmail.com>
2022-01-06 14:29:25 +0100

    reservations modif

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
     resources :reservations, only: [:new, :create]
   end
   
-  resources :reservations, only: [:show, :edit, :update, :destroy]
+  resources :reservations, only: [:index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
