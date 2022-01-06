7344a508827a7c707ee658c4d5fd43979ccba563
Claire Lignon <claire.lignon@gmail.com>
2022-01-06 17:48:20 +0100

    chatroom bifbof

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
     resources :reservations, only: [:new, :create]
   end
   
-  resources :reservations, only: [:index, :show, :edit, :update, :destroy]
+  resources :reservations, only: [:index, :show, :edit, :update, :destroy] 
+    resources :chatrooms, only: [:index,:show, :new, :create] do
+      resources :messages, only: :create
+    
+  end
+
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_150342) do
+ActiveRecord::Schema.define(version: 2022_01_06_163945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,22 @@ ActiveRecord::Schema.define(version: 2022_01_05_150342) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "chatroom_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "offers", force: :cascade do |t|
@@ -87,6 +103,8 @@ ActiveRecord::Schema.define(version: 2022_01_05_150342) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "offers", "users"
   add_foreign_key "reservations", "offers"
   add_foreign_key "reservations", "users"
