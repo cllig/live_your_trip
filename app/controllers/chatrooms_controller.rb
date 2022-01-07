830c0d1aa42ac14b2eceb46b2f186d755c552bfb
Claire Lignon <claire.lignon@gmail.com>
2022-01-07 15:28:42 +0100

    chat et params recherche

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,32 +1,39 @@
 class ChatroomsController < ApplicationController
   
-  def index
-    @chatrooms = Chatroom.all
-  end
+  # def index
+  #   @chatrooms = Chatroom.all
+  # end
 
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    @chatrooms = Chatroom.all
   end
 
-  def new
-    @chatroom = Chatroom.new
-  end
+  # def new
+  #   if request.referrer.split("/").last == "chatrooms"
+  #     flash[:notice] = nil
+  #   end
+  #   @chatroom = Chatroom.new
+  # end
 
-  def create
-    @chatroom = Chatroom.new(params_chatroom)
-    @chatroom.name = @user
-    @chatroom.user = current_user
-    if @chatroom.save
-      redirect_to chatroom_path(@reservation)
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @chatroom = Chatroom.new(chatroom_params)
+  #   if @chatroom.save
+  #     respond_to do |format|
+  #       format.html { redirect_to @chatroom }
+  #     end
+  #   else
+  #     respond_to do |format|
+  #       flash[:notice] = {error: ["a chatroom with this topic already exists"]}
+  #       format.html { redirect_to new_chatroom_path }
+  #     end
+  #   end
+  # end
   
   private
 
   def params_chatroom
-    
+    params.require(:chatroom).permit(:name)
   end
 end
