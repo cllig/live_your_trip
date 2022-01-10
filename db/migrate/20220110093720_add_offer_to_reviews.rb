02236cd0857ae963f77e04bc682448370184c805
laure reina <laure.reina@gmail.com>
2022-01-10 17:55:13 +0100

    heure ok

---
+++ db/migrate/20220110093720_add_offer_to_reviews.rb
@@ -0,0 +1,5 @@
+class AddOfferToReviews < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :reviews, :offer, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_10_115612) do
+ActiveRecord::Schema.define(version: 2022_01_10_163331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +82,8 @@ ActiveRecord::Schema.define(version: 2022_01_10_115612) do
     t.bigint "offer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.time "duree"
+    t.time "start_hour"
+    t.time "finish_hour"
     t.index ["offer_id"], name: "index_reservations_on_offer_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
@@ -113,7 +114,6 @@ ActiveRecord::Schema.define(version: 2022_01_10_115612) do
     t.boolean "guide", default: false
     t.integer "rating_number"
     t.text "rating_content"
-    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
