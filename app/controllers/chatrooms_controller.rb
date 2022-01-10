45280e7456c83cb55669d9868142853fc4ef3053
Claire Lignon <claire.lignon@gmail.com>
2022-01-10 12:34:45 +0100

    trad et autres trucs

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -16,8 +16,8 @@ class ChatroomsController < ApplicationController
 
   def create
     @chatroom = Chatroom.new(params_chatroom)
-    # @chatroom.name = @user
     # @chatroom.user = current_user
+    # @chatroom.name = @chatroom.user.first_name
     if @chatroom.save
     redirect_to @chatroom
     else
