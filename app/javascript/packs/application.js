3a507630963a349fa513c9c8f1bfe627e6c7c977
florine ponsard <florineponsard@gmail.com>
2022-01-12 17:21:30 +0100

    page détails ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,7 @@ ActiveStorage.start()
 // External imports
 import "bootstrap";
 import { initChatroomCable } from "../channels/chatroom_channel"
+import { dynamicRating } from "../plugins/init_starsReview"
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -29,4 +30,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initChatroomCable()
+  dynamicRating()
 });
